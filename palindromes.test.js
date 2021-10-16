2e024e62043a1d0c53351fc3b9caaa3dab4bae22
Christine Salim <christinesalim@gmail.com>
2021-10-16 16:36:22 -0700

    Commit more changes

---
+++ palindromes.test.js
@@ -0,0 +1,8 @@
+const palindromes = require('./palindromes');
+
+describe('palindromes()', () => {
+  it('correctly identifies one-word palindromes', () => {
+    expect(palindromes('madam')).toEqual(['madam']);
+
+  });
+});
