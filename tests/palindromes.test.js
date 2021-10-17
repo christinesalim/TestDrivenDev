647da36c4436c2290b9504fd89f535c95adba92d
Christine Salim <christinesalim@gmail.com>
2021-10-17 07:28:31 -0700

    wallaby setup

--- tests/palindromes.test.js
+++ tests/palindromes.test.js
@@ -3,6 +3,5 @@ const palindromes = require('../palindromes');
 describe('palindromes()', () => {
   it('correctly identifies one-word palindromes', () => {
     expect(palindromes('madam')).toEqual(['madam']);
-
   });
 });
